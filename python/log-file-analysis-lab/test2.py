3fcf86b3141146689a9ce1673657284d01a11ca9
chr1s77 <christine.julien@gmail.com>
2017-08-27 20:18:53 -0500

    updating test cases to python3

--- python/log-file-analysis-lab/test2.py
+++ python/log-file-analysis-lab/test2.py
@@ -6,7 +6,7 @@ num_users = 1000
 
 site_list = randomized_input.randomized_site_list(num_lines)
 user_list = randomized_input.randomized_user_list(num_lines, num_users)
-time_list = xrange(0,num_lines)
+time_list = range(0,num_lines)
 
 computed_result = compute_highest_affinity.highest_affinity(site_list, user_list, time_list)
 expected_result = ("facebook", "google")
